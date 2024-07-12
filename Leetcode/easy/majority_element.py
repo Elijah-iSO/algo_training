7f07ad6f509929acad5230d47e9aebe2749a2d9a
Илья Олейников <oleynikovis@yandex.ru>
2024-07-13 00:02:36 +0300

    one more

---
+++ Leetcode/easy/majority_element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        majority_element = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                majority_element = num
+            count += 1 if num == majority_element else -1
+
+        return majority_element
+
+
+test_case = [4,1,2,3,1,2,3,4,2,3,1,4,4]
+
+majority_element = Solution()
+print(majority_element.majorityElement(test_case))
+print(majority_element.majorityElement1(test_case))
